8eab5fbeeee4f4a265201787786ca6d888287767
Christina Boididou <Christina.Boididou@glasgow.ac.uk>
2018-02-27 12:05:40 +0000

    Add function for listing all the directories in a folder

--- find.py
+++ find.py
@@ -10,3 +10,15 @@ def find_files(folder_path):
     """
     files_list = [f for f in os.listdir(folder_path)]
     return files_list
+
+
+def find_folders(folder_path):
+    """
+    Lists all the folders in the specified folder, ignoring the files
+    :param folder_path: The path of the folder
+    :return: The list of the folders
+    """
+    folders = [directory for directory in os.listdir(folder_path)
+               if os.path.isdir(os.path.join(folder_path, directory))]
+    return folders
+
