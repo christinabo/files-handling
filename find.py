800d9a2d6209605fe8b4c25d21d8af6d2d970a9f
Christina Boididou <Christina.Boididou@glasgow.ac.uk>
2018-03-12 13:46:53 +0000

    Add function for finding recursively all the files that live in the root folder

--- find.py
+++ find.py
@@ -22,3 +22,13 @@ def find_folders(folder_path):
                if os.path.isdir(os.path.join(folder_path, directory))]
     return folders
 
+
+def find_files_recursively(folder_path):
+    """
+    Recursively lists all files that live in the root folder, ignoring the folders
+    :param folder_path: The path of the folder
+    :return: The list of the files
+    """
+    files = [filename for filename in glob.iglob(folder_path + '**/*', recursive=True)
+             if not os.path.isdir(os.path.join(folder_path, filename))]
+    return files
